f2eb11bd1bb67e10eef66e30e6553642f5317198
Brian Baboch <brian.baboch@telecom-paris.fr>
2020-05-16 09:27:44 -0700

    Checking for break inside Let

--- lab3/dragon-tiger/src/ast/binder.cc
+++ lab3/dragon-tiger/src/ast/binder.cc
@@ -147,6 +147,9 @@ void Binder::visit(Let &let) {
     }
     else{
       while(func.size() > 0){
+	if(dynamic_cast<Break *> (func[0])){
+	  utils::error("Breaks are not allowed inside Let");
+	}
         func[0]->accept(*this);
 	func.erase(func.begin());
       }

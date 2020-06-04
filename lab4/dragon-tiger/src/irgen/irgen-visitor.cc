28ce49dc6c29e449ba94aadf75d78b341bf3a900
Brian Baboch <brian.baboch@telecom-paris.fr>
2020-06-04 05:24:58 -0700

    Testing Part 5

--- lab4/dragon-tiger/src/irgen/irgen-visitor.cc
+++ lab4/dragon-tiger/src/irgen/irgen-visitor.cc
@@ -161,6 +161,9 @@ llvm::Value *IRGenerator::visit(const VarDecl &decl) {
 llvm::Value *IRGenerator::visit(const FunDecl &decl) {
   std::vector<llvm::Type *> param_types;
 
+  if(!decl.is_external) {
+    param_types.push_back(frame_up(1).first); 
+  }
   for (auto param_decl : decl.get_params()) {
     param_types.push_back(llvm_type(param_decl->get_type()));
   }

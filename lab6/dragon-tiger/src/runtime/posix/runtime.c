98bdbafb3788b615bc4d9e0174e0b1cbe6438cf2
Brian Baboch <brian.baboch@telecom-paris.fr>
2020-06-10 03:05:48 -0700

    Fixing substring

--- lab6/dragon-tiger/src/runtime/posix/runtime.c
+++ lab6/dragon-tiger/src/runtime/posix/runtime.c
@@ -84,8 +84,8 @@ const char *__substring(const char *s, int32_t first, int32_t length) {
   }
   else {
     char * myStr = (char *) malloc((length+1)*sizeof(char));
-    for(int i = 0; i < length; ++i) {
-      myStr[i] = s[first + i];
+    for(int i = first; i < first + length; ++i) {
+      myStr[i - first] = s[i];
     }
     myStr[length] = '\0';
     return myStr;

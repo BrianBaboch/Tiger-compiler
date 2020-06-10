1d395db3eed5658f20fcb9b7a040d92b059ad950
Brian Baboch <brian.baboch@telecom-paris.fr>
2020-06-10 02:49:45 -0700

    Testing concat

--- lab6/dragon-tiger/src/runtime/posix/runtime.c
+++ lab6/dragon-tiger/src/runtime/posix/runtime.c
@@ -93,12 +93,12 @@ const char *__substring(const char *s, int32_t first, int32_t length) {
 }
 
 const char *__concat(const char *s1, const char *s2) {
-  char * myStr = (char *) malloc((strlen(s1) + strlen(s2) - 1)*sizeof(char));
-  for(int i = 0; i < strlen(s1) - 1; ++i) {
+  char * myStr = (char *) malloc((strlen(s1) + strlen(s2))*sizeof(char));
+  for(int i = 0; i < strlen(s1); ++i) {
     myStr[i] = s1[i];
   }
   for(int i = 0; i < strlen(s2); ++i) {
-    myStr[strlen(s1) - 2 + i] = s2[i];
+    myStr[strlen(s1) + i] = s2[i];
   }
   return myStr;
 }
